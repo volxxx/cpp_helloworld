f0bffafc3cd234b4f05d500203627d0a033bc014
siriustom <1092245055@qq.com>
2016-12-07 23:50:39 +0800

    Update tester.cpp

--- test/tester.cpp
+++ test/tester.cpp
@@ -1,5 +1,6 @@
-//  declare an array of three  CandyBar structures that are afterwards initiated, use  new to allocate the array dynamically
+//  declare an array of three  CandyBar structures that are afterwards initiated, use new to allocate the array dynamically
 //  and then display them
+//  note the program used (*pt),pt[0],and pt-> to access specific member of object. I spent hours to figure that out.
 #include<iostream>
 #include<string>
 using namespace std;
@@ -20,15 +21,15 @@ int main()
         {"Dove",2.5,365},
         {"Valrhona",2.8,370}
     };
-    cout << "brand: " << pt[0].brand << endl;
+    cout << "brand: " << (*pt).brand << endl;
     cout << "weight: " << pt[0].weight << endl;
-    cout << "calories: " << pt[0].cal << endl << endl;
-     cout << "brand: " << pt[1].brand << endl;
+    cout << "calories: " << pt->cal << endl << endl;
+    cout << "brand: " << (*(pt+1)).brand << endl;
     cout << "weight: " << pt[1].weight << endl;
-    cout << "calories: " << pt[1].cal << "\n\n";
-     cout << "brand: " << pt[2].brand << endl;
+    cout << "calories: " << (pt+1)->cal << "\n\n";
+     cout << "brand: " << (*(pt+2)).brand << endl;
     cout << "weight: " << pt[2].weight << endl;
-    cout << "calories: " << pt[2].cal << endl;
+    cout << "calories: " << (pt+2)->cal << endl;
     delete [] pt;
     return 0;
 }
